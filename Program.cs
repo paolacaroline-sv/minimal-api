ec66697a0aa44a89ddf3235c1b91debe5daba6b8
Paola Caroline <paolacaroline-sv@outlook.com>
2025-08-06 19:52:42 -0300

    criando administrador

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
+
+
 app.MapGet("/", () => "Hello World!");
+
 app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
 {
     if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
