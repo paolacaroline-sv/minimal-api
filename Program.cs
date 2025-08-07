b7791eddab60966c5dfd34ea68c445d1f694005f
Paola Caroline <paolacaroline-sv@outlook.com>
2025-08-07 14:03:55 -0300

    GET para retornar os veiculos

--- Program.cs
+++ Program.cs
@@ -58,7 +58,11 @@ app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veic
     return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
 });
 
-
+app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico) =>
+{
+    var veiculos = veiculoServico.Todos(pagina);
+    return Results.Ok(veiculos);
+});
 
 #endregion
 
