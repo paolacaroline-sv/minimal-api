30626ca91377e0988bc83acf77d49fa2db9828df
Paola Caroline <paolacaroline-sv@outlook.com>
2025-08-07 17:53:57 -0300

    criado endpoints para administrador

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using MinimalApi.Dominio.Entidades;
 using MinimalApi.Dominio.Servicos;
 using Microsoft.AspNetCore.Mvc;
 using MinimalApi.Dominio.ModelViews;
+using MinimalApi.Dominio.Enums;
 
 
 #region Builder
@@ -43,22 +44,94 @@ app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministra
         return Results.Unauthorized();
     }
 }).WithTags("Administradores");
+
+
+app.MapGet("/administradores", ([FromQuery] int? pagina, IAdministradorServico administradorServico) =>
+{
+    var adms = new List<AdministradorModelView>();
+    var administradores = administradorServico.Todos(pagina);
+
+    foreach (var adm in administradores)
+    {
+        adms.Add(new AdministradorModelView
+        {
+            Id = adm.Id,
+            Email = adm.Email,
+            Perfil = adm.Perfil
+        });
+    }
+    return Results.Ok(adms);
+
+}).WithTags("Administradores");
+
+
+app.MapGet("/administradores/{id}", ([FromRoute] int id, IAdministradorServico administradorServico) =>
+{
+    var administrador = administradorServico.BuscaPorId(id);
+    if (administrador == null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(new AdministradorModelView
+    {
+        Id = administrador.Id,
+        Email = administrador.Email,
+        Perfil = administrador.Perfil
+    });
+}).WithTags("Administradores");
+
+
+
+
+app.MapPost("/administradores", ([FromBody] AdministradorDTO administradorDTO, IAdministradorServico administradorServico) =>
+{
+    var validacao = new ErrosDeValidacao { Mensagens = new List<string>() };
+
+    if (string.IsNullOrEmpty(administradorDTO.Email))
+        validacao.Mensagens.Add("Email não pode ser vazio");
+    if (string.IsNullOrEmpty(administradorDTO.Senha))
+        validacao.Mensagens.Add("Senha não pode ser vazia");    
+    if (validacao.Mensagens.Count > 0)
+        return Results.BadRequest(validacao);
+
+
+    var administrador = new Administrador
+    {
+        Email = administradorDTO.Email,
+        Senha = administradorDTO.Senha,
+        Perfil = administradorDTO.Perfil.ToString() ?? Perfil.Editor.ToString()
+    };
+
+    administradorServico.Incluir(administrador);
+    return Results.Created($"/administrador/{administrador.Id}", new AdministradorModelView
+    {
+        Id = administrador.Id,
+        Email = administrador.Email,
+        Perfil = administrador.Perfil
+    });
+
+}).WithTags("Administradores");
+
+
+
+
+
+
+
+
 #endregion
 
 #region Veiculos
+
 ErrosDeValidacao validaDTO(VeiculoDTO veiculoDTO)
 {
-    var validacao = new ErrosDeValidacao
-    {
-        Mensagens = new List<string>()
-    };
+    var validacao = new ErrosDeValidacao { Mensagens = new List<string>() };
 
     if (string.IsNullOrEmpty(veiculoDTO.Nome))
         validacao.Mensagens.Add("O nome não pode ficar em branco");
-
     if (string.IsNullOrEmpty(veiculoDTO.Marca))
         validacao.Mensagens.Add("A marca não pode ficar em branco");
-
     if (veiculoDTO.Ano < 1950)
         validacao.Mensagens.Add("Veículo muito antigo, aceito somente anos superiores a 1950");
 
